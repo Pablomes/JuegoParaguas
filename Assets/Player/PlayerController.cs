ddc6d7852d80bc3c2f123a0467e86be35f4f0fc0
MESTRE/LAPTOP <pablomestre22@gmail.com>
2022-11-16 12:37:09 +0100

    Player Features Added. Movement Ground Check

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float maxXSpeed;
+    public float maxYSpeed;
+    public float accel;
+    public float decel;
+    public float stopRange = 0.1f;
+    public float xSpeed = 0f;
+    public float ySpeed = 0f;
+    public float groundedCheckDis = 0.05f;
+
+    public bool isGrounded = true;
+    public bool canJump = true;
+
+    public Transform groundedOrigin;
+
+    public LayerMask groundLayer;
+
+    private float xInput = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +31,90 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CheckInput();
+        SetHorizontalSpeed();
+    }
+
+    private void FixedUpdate()
+    {
+        CheckGrounded();
+
+        Move();
+    }
+
+    void CheckInput()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+    }
+
+    void SetHorizontalSpeed()
+    {
+        if (xInput != 0)
+        {
+            if (xInput > 0)
+            {
+                if (xSpeed < 0)
+                {
+                    xSpeed = 0 + accel * Time.deltaTime;
+                }
+                if (xSpeed < maxXSpeed * xInput)
+                {
+                    xSpeed += accel * Time.deltaTime;
+                }
+                else
+                {
+                    xSpeed = maxXSpeed * xInput;
+                }
+            }
+            else
+            {
+                if (xSpeed > 0)
+                {
+                    xSpeed = 0 + accel * Time.deltaTime;
+                }
+                if (xSpeed > maxXSpeed * xInput)
+                {
+                    xSpeed -= accel * Time.deltaTime;
+                }
+                else
+                {
+                    xSpeed = maxXSpeed * xInput;
+                }
+            }
+        }
+        else
+        {
+            if (xSpeed > stopRange)
+            {
+                xSpeed -= decel * Time.deltaTime;
+            }
+            else if (xSpeed < -stopRange)
+            {
+                xSpeed += decel * Time.deltaTime;
+            }
+            else
+            {
+                xSpeed = 0f;
+            }
+        }
+    }
+
+    Vector3 CreateMovVector()
+    {
+        //IF SLOPES ARE IMPLEMENTED MAKE SURE TO CHANGE THE X SPEED TO ALSO AFFECT THE Y SPEED
+        Vector3 mov = new Vector3(xSpeed, ySpeed, 0f);
+        return mov;
+    }
+
+
+    void Move()
+    {
+        Vector3 mov = CreateMovVector();
+        gameObject.transform.Translate(mov);
+    }
+
+    void CheckGrounded()
+    {
+        isGrounded = Physics.Raycast(groundedOrigin.position, -gameObject.transform.up, groundedCheckDis, groundLayer);
     }
 }
